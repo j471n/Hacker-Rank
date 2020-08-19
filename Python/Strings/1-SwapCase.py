b21a41e378d6eda02c5d739612dab6fa6d2beef2
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-08-19 13:43:12 +0530

    Solution of Swap Case in python (Added)

---
+++ Python/Strings/1-SwapCase.py
@@ -0,0 +1,10 @@
+#programmer : Jatin Sharma
+
+#fucntion that changes the case of the string
+def swap_case(s):
+          return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
