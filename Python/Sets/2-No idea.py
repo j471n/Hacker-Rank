c563c49226e49996d6e4b4dcec1bf186ce64f5b5
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-08-20 13:11:10 +0530

    Solution of No idea in Python (Added)

---
+++ Python/Sets/2-No idea.py
@@ -0,0 +1,15 @@
+#programmer : Jatin Sharma
+class Main:
+    def __init__(self):
+        self.n, self.m = map(int, input().split())
+        self.a = list(map(int, input().split()))
+        self.b = set(map(int, input().split()))
+        self.c = set(map(int, input().split()))
+
+    def output(self):
+        print(sum([(i in self.b) - (i in self.c) for i in self.a]))
+
+
+if __name__ == '__main__':
+    obj = Main()
+    obj.output()
