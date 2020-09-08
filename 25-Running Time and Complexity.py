605e872088b8ec9c0932ef771c2fc3953b33d152
Jatin sharma <jatinsharma089659@gmail.com>
2020-09-08 16:18:19 +0530

    Adding Running Time and Complexity
    The Solution of Running Time and Complexity in Python

---
+++ 25-Running Time and Complexity.py
@@ -0,0 +1,25 @@
+#Programmer : Jatin Sharma
+
+
+from math import sqrt
+from sys import stdin
+
+
+def checkPrime(n):
+    for i in range(2, int(sqrt(n))+1):
+        if n % i is 0:
+            return False
+    return True
+
+
+def main():
+    n = int(input())
+    for line in stdin:
+        val = int(line)
+        if (val >= 2 and checkPrime(val)):
+            print("Prime")
+        else:
+            print("Not prime")
+
+if __name__ == "__main__":
+    main()
