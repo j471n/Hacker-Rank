4f2c52d2113e0051e1b6fa75e472a2b66453cf74
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-08 17:11:15 +0530

    Adding Hello,world in js

---
+++ 10 Days of JavaScript/Day-0/1-Hello,world!.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log("Welcome to 10 Days of JavaScript!");
+
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    greeting(parameterVariable);
+}
