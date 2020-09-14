75ca5f941b1f8d14bb498ac7eca4c3260c44abed
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-09-14 20:59:20 +0530

    Adding 'Type of Triangles' in SQL

---
+++ SQL/2-Advance Select/00_Type of Triangle.sql
@@ -0,0 +1,11 @@
+/*
+ For MySQL
+ */
+ 
+SELECT (CASE
+            WHEN A + B <= C OR B + C <= a OR C + A <= B THEN 'Not A Triangle'
+            WHEN A = B AND B = C AND C = A THEN 'Equilateral'
+            WHEN A = B OR B = C OR C = A THEN 'Isosceles'
+            ELSE 'Scalene'
+        END)
+FROM TRIANGLES;
