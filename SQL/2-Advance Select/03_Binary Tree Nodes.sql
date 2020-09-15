9ebd83b451990ef26b8ff9404c0fbdbf6340f3b9
Jatin sharma <jatinsharma089659@gmail.com>
2020-09-15 14:56:22 +0530

    Adding 'Binary Tree Nodes' in SQL

---
+++ SQL/2-Advance Select/03_Binary Tree Nodes.sql
@@ -0,0 +1,12 @@
+/*
+For MySQL
+*/
+
+SELECT N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST
+ORDER BY N;
