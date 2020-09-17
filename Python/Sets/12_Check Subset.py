1150f0d29ef35227a2cb25835de908e8084cf4ad
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-17 22:50:49 +0530

    Adding 'Check Subset' in Python

---
+++ Python/Sets/12_Check Subset.py
@@ -0,0 +1,9 @@
+# Programmer : Jatin Sharma
+
+for i in range(int(input())):
+    a = int(input())
+    A = set(input().split()) 
+    b = int(input())
+    B = set(input().split())
+
+    print(A.issubset(B))    #A <= B
