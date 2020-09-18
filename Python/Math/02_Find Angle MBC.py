b6083161c5991c971e9c5b9669f3241c26c79f53
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-18 12:13:14 +0530

    Adding 'Find Angle MBC' in Python

---
+++ Python/Math/02_Find Angle MBC.py
@@ -0,0 +1,9 @@
+# Programmer : Jatin Sharma
+
+import math
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(str(int(round(math.degrees(math.atan2(a, b))))) + '°')
