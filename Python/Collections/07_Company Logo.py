80e83bf9c89dbb821065cf013ce17115a9f2060e
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-19 13:39:43 +0530

    Adding 'Company Logo' in Python

---
+++ Python/Collections/07_Company Logo.py
@@ -0,0 +1,9 @@
+# Programmer : Jatin Sharma
+
+from collections import Counter, OrderedDict
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+
+[print(*i) for i in OrderedCounter(sorted(input())).most_common(3)]
+
