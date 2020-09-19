b302a40929fc43c180f41a850f711d0a32c1effc
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-19 13:42:31 +0530

    Adding 'Piling Up!' in Python

--- Python/Collections/08_Piling Up!.py
+++ Python/Collections/08_Piling Up!.py
@@ -1,1 +1,15 @@
+# Programmer : Jatin Sharma
 
+from collections import deque
+
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    a = deque(map(int, input().split()))
+    b = 'Yes'
+    if max(a) not in (a[0], a[-1]):
+        b = 'No'
+    print(b)
