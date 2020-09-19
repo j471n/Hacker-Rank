3f8ad611de0dc8a0bf77856274f755780e0d1c55
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-19 14:25:57 +0530

    Adding 'Athlete Sort' in Python

---
+++ Python/Built-Ins/04_Athlete Sort.py
@@ -0,0 +1,14 @@
+# Programmer : Jatin Sharma
+
+n, m = map(int, input().split())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+
+k = int(input())
+
+arr.sort(key = lambda x : x[k])
+for i in arr:
+    print(*i, sep=' ')
+
