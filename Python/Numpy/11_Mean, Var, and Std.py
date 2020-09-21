4aeb22cbcc13a2546294a272df1d9ba1e6fbc2d4
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-21 11:49:59 +0530

    Adding 'Mean, Var, and Std' in Python

---
+++ Python/Numpy/11_Mean, Var, and Std.py
@@ -0,0 +1,9 @@
+# Programmer : Jatin Sharma
+
+import numpy as np
+
+np.set_printoptions(legacy='1.13')
+n, m = map(int,input().split())
+a = np.array([input().split() for _ in range(m)],int)
+
+print(np.mean(a,axis=1),np.var(a,axis=0),np.std(a), sep='\n')
