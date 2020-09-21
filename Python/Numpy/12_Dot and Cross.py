5b1c39ea96622e7489a85204883a20e6bf06fec7
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-21 11:51:38 +0530

    Adding 'Dot and Cross' in Python

---
+++ Python/Numpy/12_Dot and Cross.py
@@ -0,0 +1,9 @@
+# Programmer : Jatin Sharma
+
+import numpy as np
+
+n = int(input())
+a = np.array([input().split() for i in range(n)], int)
+b = np.array([input().split() for i in range(n)], int)
+
+print(np.dot(a, b))
