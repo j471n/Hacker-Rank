f9f174ea21a5b88b4e2d6102770757c28df7421e
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-22 14:27:15 +0530

    Adding 'Basic Data Types' in C++

---
+++ C++/01_Introduction/03_Basic Data Types.cpp
@@ -0,0 +1,23 @@
+// Programmer : Jatin Sharma
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(2) << d << endl;
+    cout << fixed << setprecision(5) << e << endl;
+    return 0;
+}
