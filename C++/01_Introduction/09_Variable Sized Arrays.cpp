170b844513dde51e6bb85837191004377b88a7ef
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-22 14:49:02 +0530

    Adding 'Variable Sized Arrays'  in C++

---
+++ C++/01_Introduction/09_Variable Sized Arrays.cpp
@@ -0,0 +1,31 @@
+// Programmer : Jatin Sharma
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(void)
+{
+    int n, q;
+    cin >> n >> q;
+
+    vector< vector<int> > arr(n);
+
+    for(int i = 0; i < n; i++){
+        int k, val;
+        cin >> k;
+        for(int j = 0; j < k; j++){
+            cin >> val;
+            arr[i].push_back(val);
+        }
+    }
+
+    int m, p;
+    for(int i = 0; i < q; i++){
+        cin >> m >> p;
+        cout << arr[m][p] << endl;
+    }
+
+    return 0;
+}
