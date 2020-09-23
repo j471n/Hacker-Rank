c64ef0819ab68248a598bdbf33d85debb352b095
Jatin Sharma <jatinsharma089659@gmail.com>
2020-09-23 13:44:56 +0530

    Adding 'Strings' in C++

---
+++ C++/02_Strings/03_Strings.cpp
@@ -0,0 +1,21 @@
+// Programmer : Jatin Sharma
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << a.length() << " " << b.length() << endl;
+    cout << a + b << endl;
+
+    swap(a[0], b[0]);
+    cout << a << " " << b << endl;
+
+    return 0;
+}
+
