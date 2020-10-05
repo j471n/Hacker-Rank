059d599bc545409a4ad0d03afe3bbdc5f9aa49ab
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-10-05 13:20:48 +0530

    Adding "Dynamic Array" in Python

---
+++ Data Structures/Arrays/Python/03_Dynamic Array.py
@@ -0,0 +1,18 @@
+# Date - Oct 5, 2020 By Jatin Sharma
+
+N, Q = map(int, input().split())
+lastAns = 0
+seqList = [[] for _ in range(N)]
+
+for __ in range(Q):
+    q, x, y = map(int, input().split())
+    index = (x ^ lastAns) % N
+    seq = seqList[index]
+    if q == 1:
+        seq.append(y)
+    elif q == 2:
+        size = len(seq)
+        lastAns = seq[y % size]
+        print(lastAns)
+    else:
+        raise ValueError()
